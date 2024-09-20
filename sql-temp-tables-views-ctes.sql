db5c7692c8eb1a2e4ed9f3d546f95e54a728fd9c
lidiamayor <lidiamayor2000@gmail.com>
2024-09-20 12:13:00 +0200

    lab-sql-temp-tables-views-ctes

--- sql-temp-tables-views-ctes.sql
+++ sql-temp-tables-views-ctes.sql
@@ -1,37 +1,39 @@
 -- Step 1: Create a View
-CREATE VIEW step1 AS
+CREATE VIEW customer_rental_summary AS
 SELECT 
-	c.customer_id,
-    c.first_name,
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
     c.email,
-    count(r.rental_id) as rental_count
-FROM sakila.customer c
-JOIN sakila.rental r
-	ON c.customer_id = r.customer_id
-GROUP BY c.customer_id;
-
+    COUNT(*) AS rental_count
+FROM customer AS c
+    JOIN rental AS r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id
+ORDER BY rental_count DESC;
+    
 -- Step 2: Create a Temporary Table
-CREATE TEMPORARY TABLE step2 AS
-SELECT
-	s1.customer_id,
-	SUM(p.amount) as total_paid
-FROM sakila.payment p
-JOIN sakila.step1 s1
-	ON p.customer_id = s1.customer_id
-GROUP BY s1.customer_id;
-
+CREATE TEMPORARY TABLE customer_payment_summary AS (
+    SELECT 
+        crs.customer_id,
+        SUM(p.amount) AS total_paid
+    FROM 
+        customer_rental_summary AS crs
+        JOIN rental AS r ON crs.customer_id = r.customer_id
+        JOIN payment AS p ON r.rental_id = p.rental_id
+    GROUP BY 
+        crs.customer_id
+);
 
 -- Step 3: Create a CTE and the Customer Summary Reportcustomers
-WITH step3 AS(
-	SELECT
-		s1.first_name,
-		s1.email,
-		s1.rental_count,
-        s2.total_paid
-    FROM sakila.step1 s1
-    JOIN sakila.step2 s2 ON s1.customer_id = s2.customer_id
-)
+WITH customer_summary_report AS(
+	SELECT 
+		crs.customer_name,
+		crs.email,
+		crs.rental_count,
+		cps.total_paid
+	FROM customer_rental_summary AS crs
+	JOIN customer_payment_summary AS cps ON crs.customer_id = cps.customer_id)
 
-SELECT *, 
-	(s3.total_paid/s3.rental_count) as average_payment_per_rental
-FROM step3 s3;
+SELECT *,
+    total_paid / rental_count AS average_payment_per_rental
+FROM customer_summary_report
+ORDER BY rental_count DESC;
